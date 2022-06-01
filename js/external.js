9117b4aa85d4b227357eaf05c15efe1f14524ef9
Ashley Martinez <ashley.martinez32@gmail.com>
2022-06-01 17:30:16 -0500

    exercise 3 commit

---
+++ js/external.js
@@ -0,0 +1,13 @@
+console.log("Hello from JavaScript!")
+alert("Welcome to my Website")
+var question = prompt("What is your favorite color?")
+alert(question + " is my favorite color too")
+
+
+var mermaid = prompt("How many days was the Little Mermaid rented?")
+var p = 3
+alert("$" + mermaid * p + " is the total")
+var bear = prompt("How many days was the Brother Bear rented?")
+alert("$" + bear * p + " is the total")
+var herc = prompt("How many days was Hercules rented?")
+alert("$" + herc * p + " is the total")
