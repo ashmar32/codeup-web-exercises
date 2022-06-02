aaf4203839c5b094af807a09236aae1c0c4fe1c1
Ashley Martinez <ashley.martinez32@gmail.com>
2022-06-02 16:53:31 -0500

    functions need to finish prompts and alerts

--- js/external.js
+++ js/external.js
@@ -5,7 +5,7 @@ alert(question + " is my favorite color too")
 
 
 var mermaid = prompt("How many days was the Little Mermaid rented?")
-var p = 3
+let p = 3;
 alert("$" + mermaid * p + " is the total")
 var bear = prompt("How many days was the Brother Bear rented?")
 alert("$" + bear * p + " is the total")
@@ -22,12 +22,10 @@ let totalAmazon = prompt("Hey, How many hours did you work for Amazon?")
 let totalfaceBook = prompt("Hey, How many hours did you work for Facebook?")
 
 let sumGoogle = googlePay * totalGoogle
-alert("You will earn $" +sumGoogle + " from working at Google. You'll earn $" + (totalAmazon * amazonPay) + "from amazon. Finally, you'll earn $" + (totalfaceBook * facebookPay) + "from facebook." +
-    let timeInSchedule = confirm("Hey do you have time in your schedule for this class?")
+alert("It meets this criteria" + (moreThanTwo && offerStillValid || premiumMember) + " that you get a hat and discount")alert("You will earn $" +sumGoogle + " from working at Google. You'll earn $" + (totalAmazon * amazonPay) + "from amazon. Finally, you'll earn $" + (totalfaceBook * facebookPay) + "from facebook." + let timeInSchedule = confirm("Hey do you have time in your schedule for this class?")
 let roomInClass = confirm("Did the class have room for an additional student?")
 alert("It is " + (roomInClass && timeInSchedule) + " that you can attend this class with what you told me")
 
 let moreThanTwo = confirm("Do you have more than two items in your cart?")
 let offerStillValid = confirm("Is offer on the product still valid?")
 let premiumMember = confirm("Are you a premium member?")
-alert("It meets this criteria" + (moreThanTwo && offerStillValid || premiumMember) + " that you get a hat and discount")
---
+++ js/functionsLecture.js
@@ -0,0 +1,68 @@
+(function()
+{
+
+
+function helloWorld() {
+    //this is the body of the function - we write our instructions
+    alert("Hello World!");
+}
+//to use a function: We 'call it
+helloWorld()
+
+//A more advanced function with some parameters to allows for arguments [meaning - a place for inuote to come into the function of some kind]
+
+function sumNums(x, y){//Now - inside our parenthesis we have TWO PARAMETERS allowing our function to take in TWO PIECES of information
+    return x + y
+
+}//calling a function with arguments - look at how we don't have to re-write [repeat] ourselves
+//anonymous functions - they are still set up rather similarly but lack the name/identifier piece
+
+//the below is called a function expression vs a function declaration [ * VOCAB PIECE * }
+let honkHorn = function(){
+    alert("Beep beep!");
+}
+
+//Hoisting - who dat?
+// hoistFunction(); Weird - I can call this declared function BEFORE [above] it seems ti to be defined. <-- we're hoisting
+
+function hoistFunction(){
+    alert("Help! I've been hoisted! Where am I? I think I ended up at the top of the code :0")
+}
+//an expression does not like to hoist :(
+let duckCall = function (){
+    alert("Insert duck sound here - QUACK")
+}
+duckCall()
+
+//global scope
+
+let globalVar = "hi, I'm globally scoped ^_^";
+//function(){
+    //alert(globalVar)
+
+//function scopedComparison(){
+    let localVar = "Look I'm a local guy!"
+    alert(localVar);
+    alert(globalVar);
+
+}
+
+//scopedComparison();
+
+alert(localVar)
+
+//a lil more scoped
+let x = 300;
+let y = 100;
+
+function scopeExample(){
+    let x = 1;
+    let x = 2;
+
+    console.log("x: " + x + " | y:" + y);
+
+    return x + y;
+
+}
+})();
+
