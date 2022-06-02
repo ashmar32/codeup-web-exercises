b0c033451d2cc920d761a947633df920d1ab1187
Ashley Martinez <ashley.martinez32@gmail.com>
2022-06-02 10:42:48 -0500

    finished ex 3

--- js/external.js
+++ js/external.js
@@ -11,3 +11,23 @@ var bear = prompt("How many days was the Brother Bear rented?")
 alert("$" + bear * p + " is the total")
 var herc = prompt("How many days was Hercules rented?")
 alert("$" + herc * p + " is the total")
+
+
+let googlePay = 400
+let amazonPay = 380
+let facebookPay = 350
+
+let totalGoogle = prompt("Hey, How many hours did you work for Google?")
+let totalAmazon = prompt("Hey, How many hours did you work for Amazon?")
+let totalfaceBook = prompt("Hey, How many hours did you work for Facebook?")
+
+let sumGoogle = googlePay * totalGoogle
+alert("You will earn $" +sumGoogle + " from working at Google. You'll earn $" + (totalAmazon * amazonPay) + "from amazon. Finally, you'll earn $" + (totalfaceBook * facebookPay) + "from facebook." +
+    let timeInSchedule = confirm("Hey do you have time in your schedule for this class?")
+let roomInClass = confirm("Did the class have room for an additional student?")
+alert("It is " + (roomInClass && timeInSchedule) + " that you can attend this class with what you told me")
+
+let moreThanTwo = confirm("Do you have more than two items in your cart?")
+let offerStillValid = confirm("Is offer on the product still valid?")
+let premiumMember = confirm("Are you a premium member?")
+alert("It meets this criteria" + (moreThanTwo && offerStillValid || premiumMember) + " that you get a hat and discount")
