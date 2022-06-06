77529ac2bf4b0ad1a5dfbd7a23281c3fcb00f6b2
Ashley Martinez <ashley.martinez32@gmail.com>
2022-06-06 16:50:33 -0500

    1st problem in progress

---
+++ js/for_loops.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+// For example, showMultiplicationTable(7) should output
+
+function showMultiplicationTable(x,y){
+    let sum = (x * y)
+}
