71d6a9f471d7c6483038bb09bda0846930c87b4f
Ashley Martinez <ashley.martinez32@gmail.com>
2022-06-07 09:29:55 -0500

    Starting break and continue

---
+++ js/break_and_continue.js
@@ -0,0 +1,4 @@
+// Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+//     Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+prompt("Please enter a number between 1 - 50. ")
