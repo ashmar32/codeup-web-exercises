9a7728d9b8c301c8c6e1496b54d26f85866e0b86
Ashley Martinez <ashley.martinez32@gmail.com>
2022-06-08 20:10:32 -0500

    completed do while loop

--- js/while.js
+++ js/while.js
@@ -1,7 +1,22 @@
 "use strict";
 
-let i = 2;
-while (i < 131072) {
+let i = 1;
+while (i < 65536) {
+    i = i * 2
     console.log(i);
-    i = i * 2;
 }
+
+let allCones = Math.floor(Math.random() * (100 - 50)) + 50;
+
+do {
+   let conesSold = Math.floor(Math.random() * 5) + 1; //this is how much the customer buys 1-5
+
+    if (allCones >= conesSold) {
+        allCones = allCones - conesSold; // This sets up the formula for the total (allCones) amount of cones - the amt sold (conesSold).
+        console.log(conesSold + " cones sold! I now have " + allCones);
+    } else {
+        console.log("Sorry, I only have " + allCones + " left. I can't sell you " + conesSold + ".")
+    }
+}   while (allCones !== 0)
+
+console.log("We've sold out all our cones! Come again!")
