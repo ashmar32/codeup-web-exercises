e3cc724a36a1ed3ca4645a3df8d19bbd71ad9a30
Ashley Martinez <ashley.martinez32@gmail.com>
2022-06-10 08:59:45 -0500

    need to complete exercies

--- js/planets-array.js
+++ js/planets-array.js
@@ -20,16 +20,16 @@
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
-
+    planets.unshift("The Sun");
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
-
+    planets.push("Pluto")
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
-
+    let theSun = planets.shift();
     console.log('Finding and logging the index of "Earth" in the planets array.');
 
     console.log("Reversing the order of the planets array.");
