041c54b00cf12998d7c3a58fe9b25b3836e64985
Ashley Martinez <ashley.martinez32@gmail.com>
2022-06-10 16:49:27 -0500

    Completed exercise 2

--- js/objects.js
+++ js/objects.js
@@ -11,6 +11,7 @@
      *  > console.log(person.firstName) // "Rick"
      *  > console.log(person.lastName) // "Sanchez"
      */
+
         let person = new Object();
             person.firstName = "Ashley";
             person.lastName = "Martinez";
@@ -29,6 +30,11 @@
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
 
+        person.sayHello = function (){
+            return "Hello from " + person.firstName + person.lastName + "!"
+        }
+    console.log(person.sayHello())
+
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
      * more than $200. If a shopper spends more than $200, they get a 12%
@@ -43,11 +49,11 @@
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
 
     /** TODO:
      * Create an array of objects that represent books and store it in a
