65f4f4a64d4ee748b203c77561f1ad38eabec960
Ashley Martinez <ashley.martinez32@gmail.com>
2022-07-19 00:00:54 -0500

    added open weather key

--- js/keys.js
+++ js/keys.js
@@ -1,2 +1,3 @@
 // your keys go here
 const MAPBOX_API_KEY = "pk.eyJ1IjoibWFkbWFyY29zIiwiYSI6ImNsNXF3MzMwMTEzeG8zanBjbHh0cHB3bGQifQ.bPRH-1gcYZ0RjnsfUkkd5w";
+const OPEN_WEATHER_APPID = "fefeedf388c2a6534e4565d02f602fde"
